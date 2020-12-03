6a11d8e4db978599c127c1150cf14320086c816b
Meghaj <meghajsingh13@gmail.com>
2020-12-04 02:07:08 +0530

    initial changes

---
+++ tkint.py
@@ -0,0 +1,24 @@
+import tkinter as tkt
+class Application(tkt.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tkt.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tkt.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tkt.tkt()
+app = Application(master=root)
+app.mainloop()
